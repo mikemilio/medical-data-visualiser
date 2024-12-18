248f7eb0d2e5b00f86251b78216cac869fa35fd6
mike <mikemilioritsas@gmail.com>
2024-12-19 00:56:17 +0200

    Completed catplot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,22 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df[['weight', 'height']].apply(
+    lambda x: 1 if x[0] / ( (x[1] / 100) ** 2 ) > 25 else 0, 
+    axis=1)
 
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1 )
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1 )
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['cardio'], 
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(
+        ['cardio', 'variable', 'value']).size().reset_index(name='count').rename(columns={'count': 'total'})
     
 
     # 7
@@ -27,7 +32,9 @@ def draw_cat_plot():
 
 
     # 8
-    fig = None
+    fig = sns.catplot(data=df_cat, x='variable', y='total', 
+                      hue='value', col='cardio', 
+                      kind='bar', height=4, aspect=1.5)
 
 
     # 9
